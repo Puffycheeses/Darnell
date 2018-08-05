97545e3c684e5254998054e55d6fbb2532855b33
Puffycheeses <noahheague@gmail.com>
2018-08-05 10:24:38 +1000

    Made request Asynchronous

--- modules/commands/GetWaifu.js
+++ modules/commands/GetWaifu.js
@@ -1,4 +1,4 @@
-const request = require('request');
+const base = require('../base');
 const waifu = require('./GetWaifu');
 const Discord = require('discord.js');
 
@@ -12,32 +12,17 @@ exports.GetWaifu = async function GetWaifu(name, msg) {
             msg.delete(4000)
         });
     name = name.replace(" ", "%20");
-    let headers = {
-        'User-Agent':       'Super Agent/0.0.1',
-        'Content-Type':     'application/x-www-form-urlencoded'
-    };
-
-    let options = {
-        url: 'http://anime.tiddi.es:3300',
-        method: 'POST',
-        headers: headers,
-        form: {'character': name}
-    };
-
-    request(options, function (error, response, body) {
-        let data = JSON.parse(body);
-        if (!error && response.statusCode === 200) {
-            if(data.error === true){
-                msg.channel.send("Cannot find "+name.replace("%20", " "));
-            } else {
-                const embed = new Discord.RichEmbed()
-                    .setAuthor("Darnell", data.image)
-                    .addField(data.name, data.gender, true)
-                    .addField("Rank", data.rank, true)
-                    .addField("Anime", "[" + data.anime[0][0] + "](" + data.anime[0][1] + ")")
-                    .setImage(data.image);
-                msg.channel.send({embed});
-            }
+    base.makeRequest('http://anime.tiddi.es:3300', {'character': name}).then(data => {
+        if(data.error === true){
+            msg.channel.send("Cannot find "+name.replace("%20", " "));
+        } else {
+            const embed = new Discord.RichEmbed()
+                .setAuthor("Darnell", data.image)
+                .addField(data.name, data.gender, true)
+                .addField("Rank", data.rank, true)
+                .addField("Anime", "[" + data.anime[0][0] + "](" + data.anime[0][1] + ")")
+                .setImage(data.image);
+            msg.channel.send({embed});
         }
-    })
+    });
 };
