1a67a1bc069c0be56aebf9526a6a0e4b7abf4ae9
Puffycheeses <noahheague@gmail.com>
2018-08-25 13:47:21 +1000

    Added new commands

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const Discord = require('discord.js');
 const com = require('./modules/hub');
 const keys = require('./modules/keys/keys');
+const base = require('./modules/base');
 const client = new Discord.Client();
+
 
 const odds = 200; // Chance of a gosh darnit
 const ignore = keys.ignore;
@@ -11,14 +13,18 @@ client.on('message', msg => {
     if (msg.author.bot) return;
     if (ignore.some(word => msgText.includes(word))) return;
     if (Math.floor(Math.random() * odds) === 1) { msg.channel.send('gosh darnit darnell'); return;}
-    if (/(yo darnell)( *|, *|. *)(is|did|are|should|will|can|do|have|why|was|am|if)( *|, *|. *)[a-zA-Z0-9]/.test(msgText)) {
+    if (/(yo darnell$)/.test(msgText)) com.yo(msg);
+    if (/(yo darnell)( *|, *|. *)(is|did|are|should|will|can|do|have|why|was|am|if|you|what)( *|, *|. *)[a-zA-Z0-9]/.test(msgText)) {
         com.checkCommand(msgText).then(command => {
+            console.log(`${msgText} => ${command} => ${msg.author.username}`); // Debug & log
             switch (command) {
                 case "inviteLink": com.inviteLink(msg); break;
                 case "waifuGrabber": com.waifuGrabber(msg); break;
-                case "yeahNah": com.yeahNah(msg); break;
                 case "osu": com.osu(msg); break;
-                default: com.yo(msg);
+                case "comeOutHere": com.comeOutHere(msg); break;
+                case "leave": base.leaveVoice(msg); break;
+                case "phase": base.checkPhase(msg); break;
+                default: com.yeahNah(msg); break;
             }
         });
     }
@@ -27,7 +33,7 @@ client.on('message', msg => {
 // Set basic Info
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!\nConnected to ${client.guilds.size} servers with ${client.users.size} users total`);
-    client.user.setActivity('Need for Seed 2')
+    client.user.setActivity('Rainbow Six: Seed')
 });
 
 client.login(keys.discord);
