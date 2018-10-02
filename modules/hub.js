b726dd007341eb1f01da8851858734637ab630f4
Puffycheeses <Nojay143!>
2018-10-02 17:36:26 +1000

    Added new commands

--- modules/hub.js
+++ modules/hub.js
@@ -7,6 +7,8 @@ const inviteLink = require('./commands/inviteLink');
 const osu = require('./commands/osu');
 const comeOutHere = require('./commands/comeOutHere');
 const nhentai = require('./commands/nhentai');
+const restart = require('./commands/restart');
+const mudae = require('./commands/mudae');
 
 exports.waifuGrabber = getWaifu.waifu;
 exports.inviteLink = inviteLink.inviteLink;
@@ -15,9 +17,16 @@ exports.yo = yo.yo;
 exports.osu = osu.osu;
 exports.comeOutHere = comeOutHere.comeOutHere;
 exports.nhentai = nhentai.nhentai;
+exports.restart = restart.restart;
+exports.mudae = mudae.addWaifu;
+exports.mudaeWishList = mudae.getWishList;
+exports.mudaeCheck = mudae.check;
 
 exports.checkCommand = async function (message) {
     const commands = {
+        "getWishList": ["grab", "my", "wishlist"],
+        "addWaifu": ["add ", " to my wishlist"],
+        "restart": ["!reboot"],
         "osu": ["osu", "stats"],
         "inviteLink": ["invite", "link"],
         "waifuGrabber": ["grab", "me"],
