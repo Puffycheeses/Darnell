06d4c3ff22c30299822e10ae3efd79becd7dbee8
Puffycheeses <Nojay143!>
2018-10-02 17:37:47 +1000

    Added reminder

--- modules/commands/nhentai.js
+++ modules/commands/nhentai.js
@@ -1,3 +1,7 @@
+// ########################
+// # Needs to be cleaned! #
+// ########################
+
 const base = require('../base');
 
 exports.nhentai = function (msg) {
@@ -14,23 +18,19 @@ exports.nhentai = function (msg) {
             if (tags.includes('males only')) {
                 if (tags.includes('shotacon')) {
                     msg.channel.send(`\`${data.title.pretty}\`, fuck ${msg.author.username} your despicable. This is ultra mega homo, has gay shit ***WITH KIDS***. the fuck dude...`);
-                    return
                 }
                 msg.channel.send(`\`${data.title.pretty}\` is actually really gay, Both Cross dressing and Males only? wow...`);
-                return
             }
             msg.channel.send(`\`${data.title.pretty}\` gets complicated, I don't have the brain capacity to know *if* traps are gay`);
         } else if (tags.includes('males only') || tags.includes('yaoi')) {
             if (tags.includes('shotacon')) {
                 msg.channel.send(`\`${data.title.pretty}\`, It is literally 100% gay children wtf`);
-                return
             }
             msg.channel.send(`\`${data.title.pretty}\` is just men haha lmao ${msg.author.username} is gay`);
-            return
         } else {
-            msg.channel.send(`Usually i'd harass people about there hentai but \`${data.title.pretty}\` doesn't seem to be gay so I don't care.`)
+            msg.channel.send(`Usually i'd harass people about their hentai but \`${data.title.pretty}\` doesn't seem to be gay so I don't care.`)
         }
-    }).catch( e => {
-        msg.channel.send(`Error \`\`\`${e}\`\`\``)
+    }).catch( function() {
+        console.log("oof")
     });
 };
