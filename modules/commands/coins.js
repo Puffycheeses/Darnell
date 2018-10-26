fb6cc0b5b2ae15503f7388e8129856ccf5c44371
Puffycheeses <Nojay143!>
2018-10-26 18:37:34 +1100

    Darnell coins base

---
+++ modules/commands/coins.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const key = require('../keys/keys');
+const Discord = require('discord.js');
+const self = require('./mudae');
+
+exports.checkCoins = async function (msg) {
+
+}
+
+exports.addCoins = async function (msg) {
+
+}
+
+exports.spendCoins = async function (msg) {
+
+}
+
+exports.tradeCoins = async function (msg) {
+
+}
