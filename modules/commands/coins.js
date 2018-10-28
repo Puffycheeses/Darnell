2995b150f6eec06951e9776bd020ee5711716886
Noah Heague <noahheague@gmail.com>
2018-10-28 20:05:11 +1100

    Fixed functions

--- modules/commands/coins.js
+++ modules/commands/coins.js
@@ -5,71 +5,57 @@ let userSchema = new mongoose.Schema({id: String, coins: Number, lastEarned: Dat
 let user = mongoose.model('coins', userSchema);
 
 
-function userExists(msg) {
+async function userExists(msg) {
     console.log("Checking if exists");
-    return new Promise((resolve) => {
-        user.find({id: msg.author.id.toString()}, function (err, users) {
-            if (users.length <= 0) {
-                resolve(false)
-            } else {
-                resolve(true)
-            }
-        })
+    user.find({id: msg.author.id.toString()}, function (err, users) {
+        return users.length > 0;
     })
 }
 
 
-function findUser(msg) {
+async function findUser(msg) {
     console.log("Finding user");
-    return new Promise((resolve) => {
-        // Assumed already checked user exists
-        user.find({id: msg.author.id.toString()}, function (err, users) {
-            resolve(users)
-        })
+    // Assumed already checked user exists
+    user.find({id: msg.author.id.toString()}, function (err, users) {
+        return users
     })
 }
 
 
-function addUser(msg) {
-    return new Promise((resolve, reject) => {
-        console.log("Adding User");
-        let newUser = new user({
-            id: msg.author.id.toString(),
-            coins: 0,
-            lastEarned: new Date()
-        });
-        if (userExists(msg)) reject("User Exists");
-        newUser.save(function (err) {
-            if (err) return console.log(err);
-            console.log("Inserted new user");
-            resolve(true)
-        })
+async function addUser(msg) {
+    console.log("Adding User");
+    let newUser = new user({
+        id: msg.author.id.toString(),
+        coins: 0,
+        lastEarned: new Date()
+    });
+    if (userExists(msg)) reject("User Exists");
+    newUser.save(function (err) {
+        if (err) return console.log(err);
+        console.log("Inserted new user");
+        return true
     })
 }
 
 
-exports.checkCoins = function (msg) {
-    return new Promise((resolve) => {
-        console.log("Checking coins");
-        if (!userExists(msg)) addUser(msg);
-        findUser(msg).then(user => {
-            msg.channel.send(`You have ${user.coins.toString()} coins`);
-            resolve(true)
-        })
+exports.checkCoins = async function (msg) {
+    console.log("Checking coins");
+    if (!userExists(msg)) addUser(msg);
+    findUser(msg).then(user => {
+        msg.channel.send(`You have ${user.coins.toString()} coins`);
+        return true
     })
 };
 
-exports.addCoins = function (msg, amount) {
-    return new Promise((resolve) => {
-        console.log("Adding coins");
-        if (!userExists(msg)) addUser(msg);
-        findUser(msg).then(user => {
-            user[0].coins += amount;
-            user[0].save(function (err) {
-                if (err) return console.log(err);
-                msg.channel.send(`You have earned ${amount} darnell coins`);
-                resolve(true)
-            })
+exports.addCoins = async function (msg, amount) {
+    console.log("Adding coins");
+    if (!userExists(msg)) addUser(msg);
+    findUser(msg).then(user => {
+        user[0].coins += amount;
+        user[0].save(function (err) {
+            if (err) return console.log(err);
+            msg.channel.send(`You have earned ${amount} darnell coins`);
+            return true
         })
     })
 };
