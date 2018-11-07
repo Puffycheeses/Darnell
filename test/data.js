125a5b4cfdabae68c96d08e4e1165377fb4be848
Noah Heague <noahheague@gmail.com>
2018-11-07 14:31:48 +1100

    Fake message object for test usage

---
+++ test/data.js
@@ -0,0 +1,46 @@
+exports.Message = class {
+    constructor(content) {
+        this.deleted = false;
+        this.id = '507336109127368716';
+        this.type = 'DEFAULT';
+        this.content = content;
+        this.author = {
+            id: '173015228106145792',
+            username: 'Puffycheeses',
+            discriminator: '0001',
+            avatar: 'bc161501191c2466e3732d10166f75d8',
+            bot: false,
+            lastMessageID: '507336109127368716',
+            lastMessage: []
+        };
+        this.guild = {
+            members: [],
+            channels: [],
+            roles: [],
+            presences: [],
+            deleted: false,
+            available: true,
+            id: '505272688026320908',
+            name: 'Darnell',
+            icon: '08a6cf1bffbcc3b795868c3fd71c3772',
+            splash: null,
+            region: 'sydney',
+            memberCount: 14,
+            large: false,
+            features: [],
+            applicationID: null,
+            afkTimeout: 300,
+            afkChannelID: null,
+            systemChannelID: null,
+            embedEnabled: undefined,
+            verificationLevel: 1,
+            explicitContentFilter: 0,
+            mfaLevel: 0,
+            joinedTimestamp: 1540536849214,
+            defaultMessageNotifications: 'MENTIONS',
+            ownerID: '173015228106145792',
+            _rawVoiceStates: [],
+            emojis: []
+        }
+    }
+}
